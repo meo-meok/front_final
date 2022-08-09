774412b0714c6d9bb3c95a4187aa4492d8cd8e08
Joel Shin <joelshin@Joels-MacBook-Pro.local>
2022-08-09 16:15:48 +0900

    API

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import styled from "styled-components";
 import Header from "./header";
+import MapArea from "./MapArea";
 
 const Layout = () => {
     return (
         <div>
             <Header />
+            <MapArea />
         </div>
     )
 }

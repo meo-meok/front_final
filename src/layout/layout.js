eb9f0dd02c88904326a97ce7931c4a6628ae8941
JeeSeoyoung <jiseo09863@gmail.com>
2022-08-09 16:20:13 +0900

    hidden div add

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,12 +1,21 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 import Header from "./header";
+import Place_list from "./place_list";
+import Review from "./review";
+
+const Container = styled.div`
+padding:0;
+display:flex;
+flex-direction:column;
+`;
 
 const Layout = () => {
     return (
-        <div>
+        <Container>
             <Header />
-        </div>
+            {/* <Review /> */}
+        </Container>
     )
 }
 export default Layout

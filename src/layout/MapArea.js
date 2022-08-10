bea8c61f2c899b44ca1c18025d97a88aa712172e
Onseted <wjswls1277@gmail.com>
2022-08-10 16:52:39 +0900

    검색/깃병합

--- src/layout/MapArea.js
+++ src/layout/MapArea.js
@@ -15,6 +15,7 @@ function MapArea({ keyword }){
       if (status === kakao.maps.services.Status.OK) {
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
+        //ㅎ
         const bounds = new kakao.maps.LatLngBounds()
         let markers = []
 

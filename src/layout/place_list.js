805b16855e3137b7670b5fc86b0b827edcb0c1ee
JangJaeGeun <jaekeun950@gmail.com>
2022-08-22 08:25:29 +0900

    카테고리 갯수 수정

--- src/layout/place_list.js
+++ src/layout/place_list.js
@@ -67,7 +67,7 @@ function NavButton ({Names,setCategoryId}){
 }
 
 const PlaceList = ({setCateId}) => {
-    const NAMES = {1:"한식/분식",2:"돈까스/회/일식",3:"중식",4:"양식",5:"아시안"}
+    const NAMES = {1:"한식/분식", 2:"돈까스/회/일식", 3:"중식", 4:"양식", 5:"아시안", 6:"고기/구이", 7:"닭/치킨", 8:"찜/탕/찌개", 9:"패스트푸드", 10:"카페/디저트", 11:"호프/주류"}
     const [categoryId,setCategoryId]=useState(1);
     setCateId(categoryId)
     
